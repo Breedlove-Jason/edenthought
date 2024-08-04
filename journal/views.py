dacfdf256b10eeaae439f45933fe3a48cabefac8
Jason Breedlove <j@jasonbreedlove.com>
2024-08-03 19:51:34 -0700

    finish register, login, and logout
    very minimal styling at this point

--- journal/views.py
+++ journal/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from .forms import CreateUserForm
+
+from .forms import CreateUserForm, LoginForm
 
 
 def homepage(request):
@@ -19,7 +21,23 @@ def register(request):
 
 
 def my_login(request):
-    return render(request, "journal/my-login.html")
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("dashboard")
+    context = {"LoginForm": form}
+    return render(request, "journal/my-login.html", context)
+
+
+def user_logout(request):
+    logout(request)
+    return render(request, "journal/index.html")
 
 
 def dashboard(request):
